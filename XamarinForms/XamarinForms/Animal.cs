a68807d46526e2de8a592d2d3035f0677b974910
Sivanraj M <m.sivanraj@gmail.com>
2016-07-27 12:47:20 +0530

    Add project files.

---
+++ XamarinForms/XamarinForms/Animal.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinForms
+{
+    public class Animal
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+
+    public class AnimalsViewModel
+    {
+        public ObservableCollection<Animal> Animals { get; } = new ObservableCollection<Animal>();
+
+        public async Task GetAnimals()
+        {
+            try
+            {
+                var client = new HttpClient();
+                var json = await client.GetStringAsync("http://testapiapp360.azurewebsites.net/api/values");
+                var items = JsonConvert.DeserializeObject<List<Animal>>(json);
+                foreach (var item in items)
+                {
+                    Animals.Add(item);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
+        }
+    }
+}
---
+++ XamarinForms/XamarinForms/AnimalPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamarinForms
+{
+    public partial class AnimalPage : ContentPage
+    {
+        public AnimalPage()
+        {
+            AnimalsViewModel vm;
+            InitializeComponent();
+            BindingContext = vm = new AnimalsViewModel();
+
+            btnAnimals.Clicked += async (sender, e) =>
+            {
+                btnAnimals.IsEnabled = false;
+
+                await vm.GetAnimals();
+
+                btnAnimals.IsEnabled = true;
+            };
+        }
+    }
+}
